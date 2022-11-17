4ad4f5827bdb4519f879913613e1565d1c2faee7
Kim Yuseong <ivneld@naver.com>
2022-11-18 00:38:27 +0900

    week11

---
+++ week11/ex11-2.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct arrGraph {
+    int **arr;
+    int *label;
+    int verNum;
+} ArrGraph;
+
+ArrGraph *initGraph(int n){
+    ArrGraph *graph = (ArrGraph *) malloc(sizeof(ArrGraph));
+    graph->arr = (int **) malloc(sizeof(int *) * n);
+    for (int i = 0; i < n; i++) {
+        graph->arr[i] = (int *) malloc(sizeof(int) * n);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            graph->arr[i][j] = 0;
+    }
+    graph->label = (int *) malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        graph->label[i] = 0;
+
+
+    graph->verNum = n;
+
+    return graph;
+}
+
+void addIncidence(ArrGraph* graph, int start, int end){
+    graph->arr[start - 1][end - 1] = 1;
+    graph->arr[end - 1][start - 1] = 1;
+}
+
+void rprint(ArrGraph* graph, int s){
+    if (graph->label[s - 1] == 0) {
+        printf("%d\n", s);
+        graph->label[s - 1] = 1;
+    }
+
+    for (int i = 0; i < graph->verNum; i++) {
+        if (graph->arr[s - 1][i] == 1 && graph->label[i] == 0) {
+            printf("%d\n", i + 1);
+        }
+    }
+
+    for (int i = 0; i < graph->verNum; i++) {
+        if (graph->arr[s - 1][i] == 1) {
+            graph->label[i] = 1;
+            rprint(graph, i + 1);
+        }
+    }
+}
+
+void print(ArrGraph* graph, int s){
+    printf("%d\n", s);
+    graph->label[s - 1] = 1;
+
+    rprint(graph, s);
+}
+
+
+int main() {
+    ArrGraph *graph = initGraph(6);
+
+    for (int i = 0; i < 6; i++)
+        printf("%d ", graph->label[i]);
+
+   addIncidence(graph, 3, 5);
+    addIncidence(graph, 1, 3);
+    addIncidence(graph, 4, 1);
+    addIncidence(graph, 2, 3);
+    addIncidence(graph, 3, 4);
+    addIncidence(graph, 6, 4);
+    addIncidence(graph, 3, 6);
+    addIncidence(graph, 1, 2);
+    addIncidence(graph, 2, 5);
+
+    print(graph, 1);
+
+    for (int i = 0; i < 6; i++)
+        printf("%d ", graph->label[i]);
+    return 0;
+}
---
+++ week11/ex11-2v2.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//queue 구현용 전역변수들
+#define MAXSIZE 100000
+int front = -1, rear = -1;
+int queue[MAXSIZE];
+
+int** root;
+int N, M, V;
+
+//QUEUE 구현
+int putqueue(int val) {
+    if ((rear + 1) % MAXSIZE == front) {    // 큐가 꽉 찼는지 확인
+        return -1;
+    }
+    queue[rear] = val;     // rear가 큐의 끝 다음의 빈공간이므로 바로 저장
+    rear = ++rear % MAXSIZE; // rear를 다음 빈공간으로 변경
+    return val;
+}
+int getqueue(void) {
+    int i;
+    if (front == rear) {                  // 큐가 비어 있는지 확인
+        return (-1);
+    }
+    i = queue[front];    // front의 값을 가져옴
+    front = ++front % MAXSIZE;   // front를 다음 데이터 요소로
+    return i;
+}
+void init_queue(void) {
+    front = rear = 0;
+}
+void clear_queue(void) {
+    front = rear;
+}
+
+//BFS 구현
+void BFSsearch() {
+    int point;
+    int pathcheck = 0;
+
+    int *path = (int*)calloc(N, sizeof(int));
+    for (int i = 0; i < N; i++) {
+        path[i] = 0;
+    }
+
+    putqueue(V-1);
+    path[V-1] = 1;
+    printf("%d\n", V);
+
+    //queue 구현시에 rear = front 면 queue가 비어있는걸로 함.
+    while (rear != front) {
+        point = getqueue();
+        for (int i = 0; i < N; i++) {
+            if ((path[i] != 1) && (root[point][i] == 1)) {
+                putqueue(i);
+                printf("%d\n", i+1);
+                path[i] = 1;
+            }
+        }
+    }
+    free(path);
+
+    return;
+}
+int main(void) {
+    int i, y,x;
+
+    scanf("%d %d %d", &N,&M,&V);
+    getchar(); //\n 받기
+
+    //2차원 배열들 동적할당
+    root = (int**)calloc(N,sizeof(int*));
+    for (i = 0; i < N; i++) {
+        root[i] = (int*)calloc(N, sizeof(int));
+    }
+
+    for (i = 0; i < M; i++) {
+        scanf("%d %d",&y,&x);
+        root[y-1][x-1] = 1;
+        root[x-1][y-1] = 1;
+    }
+
+    BFSsearch();
+
+    return 0;
+}
