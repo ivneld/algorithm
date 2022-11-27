ce270c4e3d7bad9f74eccbbe50a68a5db8849df3
ysmac <ivneld@naver.com>
2022-11-27 23:16:06 +0900

    week13

--- week13/ex01.c
+++ week13/ex01.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_VERTICES 100
-#define TRUE 1
-#define FALSE 0
+#define BIG_INT 10000
 
 int n,m;
 
@@ -29,6 +28,9 @@ typedef struct graphType {
     Vertex **verList;
     Edge *edgeHead;
 } Graph;
+
+Incidence **parent;
+int *dest;
 
 
 /*    graph func     */
@@ -81,6 +83,9 @@ Graph *initGraph(){
     new->edgeHead->next = NULL;
 
     new->verList = (Vertex **) malloc(sizeof(Vertex *) * MAX_VERTICES);
+
+    dest = (int *) malloc(sizeof(int) * n);
+    parent = (Incidence **) malloc(sizeof(Incidence *) * m);
 
     return new;
 }
@@ -146,26 +151,137 @@ Queue* initQueue(){
     return new;
 }
 
-void enqueue(Queue* queue, Vertex* vertex){
+Queue *enqueue(Queue* queue, Vertex* vertex){
     Node *node = newNode(vertex);
-    node->next = queue->head->next;
+    node->vertex = vertex;
     node->prev = queue->head;
+    node->next = queue->head->next;
 
     queue->head->next->prev = node;
     queue->head->next = node;
+
+    return queue;
 }
 
 Vertex *removeMin(Queue* queue){
 
+    Node *result = queue->head;
+    int min = BIG_INT;
+
+    Node *cur = queue->head->next;
+    while (cur != queue->tail) {
+        if(cur->vertex->data < min) {
+            min = cur->vertex->data;
+            result = cur;
+        }
+        cur = cur->next;
+    }
+
+    result->next->prev = result->prev;
+    result->prev->next = result->next;
+
+    return result->vertex;
+}
+
+int isEmpty(Queue *queue){
+
+    if (queue->head->next == queue->tail) {
+        return 1;
+    }
+    else return 0;
+}
+
+void replaceKey(Queue *queue, Vertex* z, int d){
+    Node *cur = queue->head->next;
+
+    while (cur != queue->tail) {
+        if (cur->vertex == z) {
+            int idx = cur->vertex->data - 1;
+            dest[idx] = d;
+            return;
+        }
+        cur = cur->next;
+    }
+    puts("replace error");
+}
+
+int isContain(Queue *queue, Vertex* vertex){
+    Node* cur=queue->head;
+
+    while (cur != queue->tail) {
+        cur = cur->next;
+        if(cur->vertex == vertex) return 1;
+    }
+    return 0;
+}
+
+Incidence *getIncidence(Vertex* u, Vertex* v){
+    Incidence *cur = u->head;
+    while (cur->next != NULL) {
+        cur = cur->next;
+        if((cur->edge->u == u && cur->edge->v == v) || (cur->edge->u == v && cur->edge->v == u))
+            return cur;
+    }
+    puts("getIncidence error");
+    return NULL;
+}
+
+int* Prim_JarnikMST(Graph* graph){
+    int *result = (int *) malloc(sizeof(int) * n);
+    int rIdx = 0;
+
+    for (int i = 0; i < n; i++) {
+        dest[i] = BIG_INT;
+        parent[i] = NULL;
+    }
+
+    dest[0] = 0;
+    result[rIdx++] = 1;
+
+    Queue *queue = initQueue();
+    for (int i = 0; i < n; i++) {
+        enqueue(queue, graph->verList[i]);
+    }
+
+    while (!isEmpty(queue)) {
+        Vertex *u = removeMin(queue);
+
+        Incidence *cur = u->head;
+        while (cur->next != NULL) {
+            cur = cur->next;
+            Vertex *z = oppositeVertex(u, cur->edge);
+
+            if (isContain(queue, z) && cur->edge->weight < dest[z->data - 1]) {
+                dest[z->data - 1] = cur->edge->weight;
+                parent[z->data - 1] = cur;
+                replaceKey(queue, z, cur->edge->weight);
+                result[rIdx++] = z->data;
+            }
+        }
+        printf("%d ", u->data);
+    }
+
+    return result;
 }
 
 int main(){
     Graph *graph = buildGraph();
 
-    Edge *cur = graph->edgeHead;
-    while (cur->next != NULL) {
-        cur = cur->next;
-        printf("%d %d %d\n", cur->u->data, cur->v->data, cur->weight);
-    }
+    int *list = Prim_JarnikMST(graph);
+
+    /*for (int i = 0; i < n; i++) {
+        printf(" %d", list[i]);
+    }*/
+
+
     return 0;
 }
+
+/*5 7
+1 2 1
+1 4 2
+1 5 4
+2 5 7
+4 5 3
+3 5 5
+2 3 6*/
